c8989519cd5866d4e97a08f4d79535b3f198002c
milosevicmateja <nejimilosevic@gmail.com>
2026-02-18 13:11:40 +0100

    Create math_utils.py

---
+++ math_utils.py
@@ -0,0 +1,15 @@
+def is_prime(n: int) -> bool:
+    if n <= 1:
+        return False
+    if n <= 3:
+        return True
+    if n % 2 == 0:
+        return False
+
+    i = 3
+    while i * i <= n:
+        if n % i == 0:
+            return False
+        i += 2
+
+    return True
